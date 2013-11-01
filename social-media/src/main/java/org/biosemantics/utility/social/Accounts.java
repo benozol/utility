ffb6cab003e5cb908b25f0fe4e18704732232cd2
Benedikt Becker <benedikt@benozol.de>
2013-11-01 15:50:47 +0100

    Read ACCESS_PROPERTIES from env

--- social-media/src/main/java/org/biosemantics/utility/social/Accounts.java
+++ social-media/src/main/java/org/biosemantics/utility/social/Accounts.java
@@ -1,6 +1,9 @@
 package org.biosemantics.utility.social;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public enum Accounts {
@@ -9,8 +12,8 @@ public enum Accounts {
 	GOOGLEPLUS_API_KEY("googleplus_api_key"),
 	
 	FACEBOOK_ACCESS_TOKEN("facebook_token"),
-	FACEBOOK_TOKEN,
-	FACEBOOK_TOKEN_SECRET,
+	FACEBOOK_TOKEN(""),
+	FACEBOOK_TOKEN_SECRET(""),
 	
 	TWITTER_CONSUMER_KEY("twitter_consumer_key"),
 	TWITTER_CONSUMER_SECRET("twitter_consumer_secret"),
@@ -18,26 +21,33 @@ public enum Accounts {
 	TWITTER_TOKEN_SECRET("twitter_token_secret"),
 	TWITTER_APPLICATION_ID("twitter_application_id");
 	
+	private static final String ACCESS_PROPERTIES_ENVVAR = "ACCESS_PROPERTIES";
+
 	private static final String ACCESS_PROPERTIES = "/access.properties";
 
-	private String value;
-	private Properties prop = new Properties();
-	{
+	private String key;
+	private static Properties prop = new Properties();
+	static {
 		try {
-			prop.load(getClass().getResourceAsStream(ACCESS_PROPERTIES));
+			
+			String accessPropertiesEnvVar = System.getenv(ACCESS_PROPERTIES_ENVVAR);
+			if (accessPropertiesEnvVar != null) {
+				System.out.println(String.format("Using %s as access.properties", accessPropertiesEnvVar));
+				InputStream stream = new FileInputStream(new File(accessPropertiesEnvVar));
+				prop.load(stream);
+				stream.close();
+			} else
+				prop.load(Accounts.class.getResourceAsStream(ACCESS_PROPERTIES));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private Accounts(String key) {
-		this.value = prop.getProperty(key);
-	}
-	private Accounts() {
-		this.value = null;
+		this.key = key;
 	}
 	public String getValue() {
-		return value;
+		return prop.getProperty(key);
 	}
 
 }
